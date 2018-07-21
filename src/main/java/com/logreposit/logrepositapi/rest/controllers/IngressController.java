e64909bc577adb89d851b02cf0befba13dc0bdb4
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-21 13:40:00 +0200

    IngressController: cosmetics

--- src/main/java/com/logreposit/logrepositapi/rest/controllers/IngressController.java
+++ src/main/java/com/logreposit/logrepositapi/rest/controllers/IngressController.java
@@ -3,13 +3,17 @@ package com.logreposit.logrepositapi.rest.controllers;
 import com.logreposit.logrepositapi.persistence.documents.Device;
 import com.logreposit.logrepositapi.rest.dtos.ResponseDto;
 import com.logreposit.logrepositapi.rest.dtos.common.SuccessResponse;
+import com.logreposit.logrepositapi.rest.dtos.request.LogIngressRequestDto;
 import com.logreposit.logrepositapi.rest.dtos.response.DeviceResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class IngressController
@@ -19,7 +23,12 @@ public class IngressController
     }
 
     @RequestMapping(path = "/ingress", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<SuccessResponse<ResponseDto>> ingress(Device device)
+    public ResponseEntity<SuccessResponse<ResponseDto>> ingress(Device device, @RequestBody @Valid LogIngressRequestDto logIngressRequestDto)
+    {
+        return new ResponseEntity<>(buildResponse(device), HttpStatus.OK);
+    }
+
+    private static SuccessResponse<ResponseDto> buildResponse(Device device)
     {
         DeviceResponseDto deviceResponseDto = new DeviceResponseDto();
 
@@ -28,6 +37,6 @@ public class IngressController
 
         SuccessResponse<ResponseDto> successResponse = SuccessResponse.builder().data(deviceResponseDto).build();
 
-        return new ResponseEntity<>(successResponse, HttpStatus.OK);
+        return successResponse;
     }
 }
