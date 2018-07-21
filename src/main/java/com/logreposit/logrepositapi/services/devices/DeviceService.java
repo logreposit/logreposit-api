18134ef491fb50224e930dddb3cd607b7c4ff5e3
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-21 16:19:39 +0200

    DeviceService: cosmetics

--- src/main/java/com/logreposit/logrepositapi/services/devices/DeviceService.java
+++ src/main/java/com/logreposit/logrepositapi/services/devices/DeviceService.java
@@ -5,6 +5,6 @@ import com.logreposit.logrepositapi.persistence.documents.Device;
 public interface DeviceService
 {
     Device create           (Device device);
-    Device get              (String userId) throws DeviceNotFoundException;
-    Device getByDeviceToken (String apiKey) throws DeviceTokenNotFoundException, DeviceNotFoundException;
+    Device get              (String deviceId) throws DeviceNotFoundException;
+    Device getByDeviceToken (String apiKey)   throws DeviceTokenNotFoundException, DeviceNotFoundException;
 }
