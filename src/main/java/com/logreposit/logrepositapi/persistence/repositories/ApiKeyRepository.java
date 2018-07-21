d77ead0d8c17c83b533786d6bef2847a8b9b4211
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-21 23:34:34 +0200

    switched from PageRequest to Pageable in repos

--- src/main/java/com/logreposit/logrepositapi/persistence/repositories/ApiKeyRepository.java
+++ src/main/java/com/logreposit/logrepositapi/persistence/repositories/ApiKeyRepository.java
@@ -2,15 +2,14 @@ package com.logreposit.logrepositapi.persistence.repositories;
 
 import com.logreposit.logrepositapi.persistence.documents.ApiKey;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ApiKeyRepository extends MongoRepository<ApiKey, String>
 {
     Optional<ApiKey> findByKey         (String key);
     Optional<ApiKey> findByIdAndUserId (String id, String userId);
-    Page<ApiKey>     findByUserId      (String userId, PageRequest pageRequest);
+    Page<ApiKey>     findByUserId      (String userId, Pageable pageable);
 }
--- src/main/java/com/logreposit/logrepositapi/persistence/repositories/DeviceRepository.java
+++ src/main/java/com/logreposit/logrepositapi/persistence/repositories/DeviceRepository.java
@@ -2,13 +2,13 @@ package com.logreposit.logrepositapi.persistence.repositories;
 
 import com.logreposit.logrepositapi.persistence.documents.Device;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
 public interface DeviceRepository extends MongoRepository<Device, String>
 {
-    Page<Device>     findByUserId      (String userId, PageRequest pageRequest);
+    Page<Device>     findByUserId      (String userId, Pageable pageable);
     Optional<Device> findByIdAndUserId (String id, String userId);
 }
