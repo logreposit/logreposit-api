0c2e9ab22ad3cfae71e6156fa91b987b33a87a25
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-22 12:52:37 +0200

    disabled favicon route and error route

--- src/main/java/com/logreposit/logrepositapi/LogrepositApiApplication.java
+++ src/main/java/com/logreposit/logrepositapi/LogrepositApiApplication.java
@@ -3,8 +3,9 @@ package com.logreposit.logrepositapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
+@SpringBootApplication(exclude = {RepositoryRestMvcAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
 public class LogrepositApiApplication
 {
     public static void main(String[] args)
