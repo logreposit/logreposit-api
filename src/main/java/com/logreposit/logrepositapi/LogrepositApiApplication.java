70627914c6bebce409ecc18e8863686b4e5c74a1
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-22 12:42:15 +0200

    sec

--- src/main/java/com/logreposit/logrepositapi/LogrepositApiApplication.java
+++ src/main/java/com/logreposit/logrepositapi/LogrepositApiApplication.java
@@ -2,8 +2,9 @@ package com.logreposit.logrepositapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class LogrepositApiApplication
 {
     public static void main(String[] args)
