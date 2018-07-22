3c214fd5c7d1736e97f261bc8bf3c43e0b9d1a43
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-22 11:46:11 +0200

    added correlationId to MDC and log pattern

--- src/main/java/com/logreposit/logrepositapi/rest/filters/RequestCorrelation.java
+++ src/main/java/com/logreposit/logrepositapi/rest/filters/RequestCorrelation.java
@@ -1,5 +1,7 @@
 package com.logreposit.logrepositapi.rest.filters;
 
+import org.slf4j.MDC;
+
 public class RequestCorrelation
 {
     public static final String CORRELATION_ID_HEADER_NAME = "x-correlation-id"; // TODO: USEFUL for other services! this one is the edge service
@@ -9,6 +11,8 @@ public class RequestCorrelation
     public static void setCorrelationId(String correlationId)
     {
         id.set(correlationId);
+
+        MDC.put("correlationId", correlationId);
     }
 
     public static String getCorrelationId()
