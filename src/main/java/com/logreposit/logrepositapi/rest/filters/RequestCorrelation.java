a36dbf02eac987ee79d67ef1e6b3ca3944d4f060
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-23 12:19:56 +0200

    RequestCorrelation: cosmetics

--- src/main/java/com/logreposit/logrepositapi/rest/filters/RequestCorrelation.java
+++ src/main/java/com/logreposit/logrepositapi/rest/filters/RequestCorrelation.java
@@ -4,8 +4,6 @@ import org.slf4j.MDC;
 
 public class RequestCorrelation
 {
-    public static final String CORRELATION_ID_HEADER_NAME = "x-correlation-id"; // TODO: USEFUL for other services! this one is the edge service
-
     private static final ThreadLocal<String> id = new ThreadLocal<>();
 
     public static void setCorrelationId(String correlationId)
