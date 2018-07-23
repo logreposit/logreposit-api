71eb3c943d9860124621a359ff95e42c4c736440
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-23 09:50:02 +0200

    IngressController: changed new response to IngressResponseDto

--- src/main/java/com/logreposit/logrepositapi/rest/controllers/IngressController.java
+++ src/main/java/com/logreposit/logrepositapi/rest/controllers/IngressController.java
@@ -5,6 +5,7 @@ import com.logreposit.logrepositapi.rest.dtos.ResponseDto;
 import com.logreposit.logrepositapi.rest.dtos.common.SuccessResponse;
 import com.logreposit.logrepositapi.rest.dtos.request.LogIngressRequestDto;
 import com.logreposit.logrepositapi.rest.dtos.response.DeviceResponseDto;
+import com.logreposit.logrepositapi.rest.dtos.response.IngressResponseDto;
 import com.logreposit.logrepositapi.services.ingress.IngressService;
 import com.logreposit.logrepositapi.services.ingress.IngressServiceException;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Date;
 
 @RestController
 @Validated
@@ -32,19 +34,23 @@ public class IngressController
     @RequestMapping(path = "/ingress", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SuccessResponse<ResponseDto>> ingress(Device device, @RequestBody @Valid LogIngressRequestDto logIngressRequestDto) throws IngressServiceException
     {
+        Date start = new Date();
+
         this.ingressService.processData(device, logIngressRequestDto.getDeviceType(), logIngressRequestDto.getData());
 
-        return new ResponseEntity<>(buildResponse(device), HttpStatus.OK); // TODO: meaningful response
+        Date now   = new Date();
+        long delta = now.getTime() - start.getTime();
+
+        return new ResponseEntity<>(buildResponse(delta), HttpStatus.OK);
     }
 
-    private static SuccessResponse<ResponseDto> buildResponse(Device device)
+    private static SuccessResponse<ResponseDto> buildResponse(long delta)
     {
-        DeviceResponseDto deviceResponseDto = new DeviceResponseDto();
+        String message = String.format("Data was successfully processed in %s milliseconds.", delta);
 
-        deviceResponseDto.setId(device.getId());
-        deviceResponseDto.setName(device.getName());
+        IngressResponseDto ingressResponseDto = new IngressResponseDto(message);
 
-        SuccessResponse<ResponseDto> successResponse = SuccessResponse.builder().data(deviceResponseDto).build();
+        SuccessResponse<ResponseDto> successResponse = SuccessResponse.builder().data(ingressResponseDto).build();
 
         return successResponse;
     }
