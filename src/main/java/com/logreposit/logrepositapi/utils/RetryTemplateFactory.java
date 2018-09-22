1cce52f60c188af865ba3ad8951cf0cc26d59851
Dominic Miglar <dominic.miglar@netunix.at>
2018-09-22 11:24:47 +0200

    cosmetics

--- src/main/java/com/logreposit/logrepositapi/utils/RetryTemplateFactory.java
+++ src/main/java/com/logreposit/logrepositapi/utils/RetryTemplateFactory.java
@@ -4,10 +4,6 @@ import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class RetryTemplateFactory
 {
     private RetryTemplateFactory()
@@ -20,27 +16,6 @@ public class RetryTemplateFactory
     {
         SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
         simpleRetryPolicy.setMaxAttempts(maxAttempts);
-
-        ExponentialBackOffPolicy exponentialBackOffPolicy = new ExponentialBackOffPolicy();
-        exponentialBackOffPolicy.setInitialInterval(initialBackOffInterval);
-        exponentialBackOffPolicy.setMultiplier(backOffMultiplier);
-
-        RetryTemplate retryTemplate = new RetryTemplate();
-        retryTemplate.setBackOffPolicy(exponentialBackOffPolicy);
-        retryTemplate.setRetryPolicy(simpleRetryPolicy);
-
-        return retryTemplate;
-    }
-
-    public static RetryTemplate createWithExponentialBackOffForGivenException(int maxAttempts,
-                                                                              long initialBackOffInterval,
-                                                                              double backOffMultiplier,
-                                                                              List<Class<? extends Throwable>> retryableExceptions)
-    {
-        Map<Class<? extends Throwable>, Boolean> retryableExceptionsAsMap = new HashMap<>();
-        retryableExceptions.forEach(exception -> retryableExceptionsAsMap.put(exception, true));
-
-        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy(maxAttempts, retryableExceptionsAsMap);
 
         ExponentialBackOffPolicy exponentialBackOffPolicy = new ExponentialBackOffPolicy();
         exponentialBackOffPolicy.setInitialInterval(initialBackOffInterval);
