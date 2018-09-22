b224ead48573a12c216f94dafd0ee3a8b21155da
Dominic Miglar <dominic.miglar@netunix.at>
2018-09-22 11:30:39 +0200

    fix

--- build/01_test.sh
+++ build/01_test.sh
@@ -4,8 +4,6 @@ set -eu
 
 current_directory="$( cd "$(dirname "$0")" ; pwd -P )"
 
-source "${current_directory}/common.sh"
-
 cd "${current_directory}/.."
 
 mvn versions:set -DnewVersion=$(git describe)
