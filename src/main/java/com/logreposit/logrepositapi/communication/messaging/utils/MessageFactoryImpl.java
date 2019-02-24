c880c84200ea011aaa8e2d34517baed8309d38a1
Dominic Miglar <dominic.miglar@netunix.at>
2019-02-24 10:35:48 +0100

    added cotek ingress processing

--- src/main/java/com/logreposit/logrepositapi/communication/messaging/utils/MessageFactoryImpl.java
+++ src/main/java/com/logreposit/logrepositapi/communication/messaging/utils/MessageFactoryImpl.java
@@ -109,6 +109,23 @@ public class MessageFactoryImpl implements MessageFactory
     }
 
     @Override
+    public Message buildEventCotekSPSeriesLogdataReceivedMessage(Object cotekLogData, String deviceId, String userId) throws JsonProcessingException
+    {
+        MessageMetaData messageMetaData = new MessageMetaData();
+        messageMetaData.setDeviceId(deviceId);
+        messageMetaData.setUserId(userId);
+
+        Message message = createMessage(messageMetaData);
+
+        message.setType(MessageType.EVENT_COTEK_SP_SERIES_LOGDATA_RECEIVED.toString());
+        message.setPayload(this.objectMapper.writeValueAsString(cotekLogData));
+
+        addCorrelationIdToMessage(message);
+
+        return message;
+    }
+
+    @Override
     public Message buildEventUserCreatedMessage(UserCreatedMessageDto user) throws JsonProcessingException
     {
         MessageMetaData messageMetaData = new MessageMetaData();
