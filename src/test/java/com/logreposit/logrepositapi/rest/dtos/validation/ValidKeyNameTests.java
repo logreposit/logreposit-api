0d823e16682078ae72c8a248613a1966dca2bab8
Dominic Miglar <dominic.miglar@netunix.at>
2020-08-06 18:33:16 +0200

    cosmetics

--- src/test/java/com/logreposit/logrepositapi/rest/dtos/validation/ValidKeyNameTests.java
+++ src/test/java/com/logreposit/logrepositapi/rest/dtos/validation/ValidKeyNameTests.java
@@ -7,7 +7,6 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
