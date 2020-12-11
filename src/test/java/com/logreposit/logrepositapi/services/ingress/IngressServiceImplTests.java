862f12acac9f64de16874361f17ce7a71bce231f
Dominic Miglar <dominic.miglar@netunix.at>
2020-12-11 18:41:07 +0100

    cosmetics

--- src/test/java/com/logreposit/logrepositapi/services/ingress/IngressServiceImplTests.java
+++ src/test/java/com/logreposit/logrepositapi/services/ingress/IngressServiceImplTests.java
@@ -170,7 +170,7 @@ public class IngressServiceImplTests
         Mockito.when(this.messageFactory.buildEventCmiLogdataReceivedMessage(Mockito.any(Object.class), Mockito.eq(device.getId()), Mockito.eq(device.getUserId())))
                .thenReturn(message);
 
-        Mockito.doThrow(new MessageSenderException("some error occurred")).when(this.messageSender).send(Mockito.eq(message));
+        Mockito.doThrow(new MessageSenderException("some error occurred", new RuntimeException())).when(this.messageSender).send(Mockito.eq(message));
 
         var e = assertThrows(IngressServiceException.class, () -> this.ingressService.processData(device, deviceType, data));
 
@@ -196,7 +196,7 @@ public class IngressServiceImplTests
         Mockito.when(this.messageFactory.buildEventGenericLogdataReceivedMessage(Mockito.any(), Mockito.eq(device.getId()), Mockito.eq(device.getUserId())))
                .thenReturn(message);
 
-        Mockito.doThrow(new MessageSenderException("some error occurred")).when(this.messageSender).send(Mockito.eq(message));
+        Mockito.doThrow(new MessageSenderException("some error occurred", new RuntimeException())).when(this.messageSender).send(Mockito.eq(message));
 
         var e = assertThrows(IngressServiceException.class, () -> this.ingressService.processData(device, readings));
 
