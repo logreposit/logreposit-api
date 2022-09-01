3bedf377b1cbf40dd303821b6ce6b5335ebccb5b
Dominic Miglar <dominic.miglar@netunix.at>
2022-09-01 11:47:35 +0200

    added account ID to get account response (controller & openApi definition file)

--- src/main/java/com/logreposit/logrepositapi/rest/dtos/response/AccountResponseDto.java
+++ src/main/java/com/logreposit/logrepositapi/rest/dtos/response/AccountResponseDto.java
@@ -1,11 +1,18 @@
 package com.logreposit.logrepositapi.rest.dtos.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.logreposit.logrepositapi.rest.controllers.AccountController;
 import com.logreposit.logrepositapi.rest.dtos.ResponseDto;
 import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @Data
 public class AccountResponseDto implements ResponseDto {
+  private final String id;
   private final String email;
+
+  public AccountResponseDto(String id, String email) {
+    this.id = id;
+    this.email = emaill;
+  }
 }
