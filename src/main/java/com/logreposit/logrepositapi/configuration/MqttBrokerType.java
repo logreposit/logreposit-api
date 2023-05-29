e42689943376e9be25436f99ff05f358b6954e5d
Dominic Miglar <dominic.miglar@netunix.at>
2023-05-29 19:49:21 +0200

    cosmetics

--- src/main/java/com/logreposit/logrepositapi/configuration/MqttBrokerType.java
+++ src/main/java/com/logreposit/logrepositapi/configuration/MqttBrokerType.java
@@ -1,6 +1,13 @@
 package com.logreposit.logrepositapi.configuration;
 
 public enum MqttBrokerType {
-  MOSQUITTO,
-  EMQX
+  MOSQUITTO(Constants.MOSQUITTO_VALUE),
+  EMQX(Constants.EMQX_VALUE);
+
+  MqttBrokerType(String value) {}
+
+  public static class Constants {
+    public static final String MOSQUITTO_VALUE = "MOSQUITTO";
+    public static final String EMQX_VALUE = "EMQX";
+  }
 }
--- src/main/java/com/logreposit/logrepositapi/services/mqtt/emqx/EmqxApiClient.java
+++ src/main/java/com/logreposit/logrepositapi/services/mqtt/emqx/EmqxApiClient.java
@@ -1,14 +1,21 @@
 package com.logreposit.logrepositapi.services.mqtt.emqx;
 
+import com.logreposit.logrepositapi.configuration.MqttConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class EmqxApiClient {
+  private final MqttConfiguration mqttConfiguration;
+
+  public EmqxApiClient(MqttConfiguration mqttConfiguration) {
+    this.mqttConfiguration = mqttConfiguration;
+  }
+
   // TODO: write implementation!
 
   public void dummyMethod() {
-    log.info("TODO: Dummy :)");
+    log.info("TODO: Dummy :) MQTT Configuration: {}", mqttConfiguration);
   }
 }
