6617183be118b565b7e2f82e108cb39671f00ffb
Dominic Miglar <dominic.miglar@netunix.at>
2023-05-29 19:12:41 +0200

    cosmetics

--- src/main/java/com/logreposit/logrepositapi/configuration/MqttConfiguration.java
+++ src/main/java/com/logreposit/logrepositapi/configuration/MqttConfiguration.java
@@ -14,8 +14,8 @@ import org.springframework.validation.annotation.Validated;
 @Getter
 @Setter
 public class MqttConfiguration {
-  @NotNull private MqttBrokerType brokerType;
   @NotNull private boolean enabled;
+  @NotNull private MqttBrokerType brokerType;
   @NotBlank private String host;
   @NotNull private Integer port;
   @NotBlank private String username;
