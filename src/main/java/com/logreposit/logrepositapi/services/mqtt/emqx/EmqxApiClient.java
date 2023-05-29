a14b6706eb07a8619a166477b9ae527d5e37116c
Dominic Miglar <dominic.miglar@netunix.at>
2023-05-29 19:05:52 +0200

    boilerplate for emqx client

---
+++ src/main/java/com/logreposit/logrepositapi/services/mqtt/emqx/EmqxApiClient.java
@@ -0,0 +1,10 @@
+package com.logreposit.logrepositapi.services.mqtt.emqx;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class EmqxApiClient {
+  // TODO
+}
