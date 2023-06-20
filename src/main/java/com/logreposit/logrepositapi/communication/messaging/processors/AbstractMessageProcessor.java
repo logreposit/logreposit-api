64c7a986720baf0c29a54cb0002dd9a69677808a
Dominic Miglar <dominic.miglar@netunix.at>
2023-06-20 18:28:24 +0200

    cosmetics

--- src/main/java/com/logreposit/logrepositapi/communication/messaging/processors/AbstractMessageProcessor.java
+++ src/main/java/com/logreposit/logrepositapi/communication/messaging/processors/AbstractMessageProcessor.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.logreposit.logrepositapi.communication.messaging.common.Message;
 import com.logreposit.logrepositapi.communication.messaging.exceptions.MessagingException;
-import com.logreposit.logrepositapi.utils.LoggingUtils;
 import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,15 +22,7 @@ public abstract class AbstractMessageProcessor<T> {
   protected T getMessagePayload(Message message, TypeReference<T> typeReference)
       throws MessagingException {
     try {
-      T payload = this.objectMapper.readValue(message.getPayload(), typeReference);
-
-      // TODO DoM: maybe get rid of those logs?!
-      logger.info(
-          "Successfully deserialized Message Payload into {} instance: {}",
-          typeReference.toString(),
-          LoggingUtils.serialize(payload));
-
-      return payload;
+      return this.objectMapper.readValue(message.getPayload(), typeReference);
     } catch (IOException exception) {
       logger.error(
           "Unable to deserialize Message payload to instance of '{}'.", typeReference.toString());
