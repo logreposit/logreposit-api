43dc31847c7c749fbeb1d553aaea56032a28a228
Dominic Miglar <dominic.miglar@netunix.at>
2023-06-22 14:45:56 +0200

    removed unused file

--- src/main/java/com/logreposit/logrepositapi/services/mqtt/MqttSupportNotEnabledException.java
+++
@@ -1,7 +0,0 @@
-package com.logreposit.logrepositapi.services.mqtt;
-
-public class MqttSupportNotEnabledException extends MqttCredentialServiceException {
-  public MqttSupportNotEnabledException() {
-    super("MQTT Support is not enabled!");
-  }
-}
