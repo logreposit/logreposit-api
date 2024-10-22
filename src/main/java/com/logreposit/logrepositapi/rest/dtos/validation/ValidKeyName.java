460d61726312d26c298d165592233f76dc6b3cb0
Dominic Miglar <dominic.miglar@netunix.at>
2024-10-22 23:17:08 +0200

    fix regex

--- src/main/java/com/logreposit/logrepositapi/rest/dtos/validation/ValidKeyName.java
+++ src/main/java/com/logreposit/logrepositapi/rest/dtos/validation/ValidKeyName.java
@@ -17,14 +17,14 @@ import java.lang.annotation.Target;
 // must end with 0-9 or a-z
 // must not be "time" (reserved keyword)
 @NotBlank
-@Pattern(regexp = "^(?!^time$)[a-z]+[0-9a-z_]*[0-9a-z]+$")
+@Pattern(regexp = "^(?!^time$)[a-z]+(?:[0-9a-z_]*[0-9a-z]+)*$")
 @Target({FIELD, TYPE_USE})
 @Retention(RUNTIME)
 @Constraint(validatedBy = {})
 @Documented
 public @interface ValidKeyName {
   String message() default
-      "Invalid name. Must match regex `^(?!^time$)[a-z]+[0-9a-z_]*[0-9a-z]+$`.";
+      "Invalid name. Must match regex `^(?!^time$)[a-z]+(?:[0-9a-z_]*[0-9a-z]+)*$`.";
 
   Class<?>[] groups() default {};
 
